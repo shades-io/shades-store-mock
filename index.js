803171ddf942e84ea3998fb5dd7abd736875c078
Alexander Biskop <abiskop@netflix.com>
2017-06-09 19:18:57 -0700

    Add "projectionStates.all" method

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ const create = options => {
 
     const putProjectionState = (namespace, projectionName, state) => {};
     const getProjectionState = (namespace, projectionName) => {};
+    const getAll = () => {
+        /*
+        Returns:
+
+        {
+            "myNamespace1": {
+                "projectionTypeA": { ... },
+                "projectionTypeB": { ... }
+            },
+            "myOtherNamespace": {
+                "projectionTypeA": { ... }
+            }
+        }
+        */
+    };
     const appendEvent = (namespace, event) => {};
 
     return {
-        projectionState: {
+        projectionStates: {
             put: putProjectionState,
-            get: getProjectionState
+            get: getProjectionState,
+            all: getAll
         },
         events: {
             append: appendEvent
