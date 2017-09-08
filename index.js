b71eef5a0b5028a143c17ecd96f1f6c14a7fb48d
Alexander Biskop <abiskop@netflix.com>
2017-09-08 16:16:29 -0700

    Implement mock storage

--- index.js
+++ index.js
@@ -3,24 +3,29 @@
 const create = options => {
     const mockDB = {};
 
-    const putProjectionState = (namespace, projectionName, state) => {};
-    const getProjectionState = (namespace, projectionName) => {};
-    const getAll = () => {
-        /*
-        Returns:
-
-        {
-            "myNamespace1": {
-                "projectionTypeA": { ... },
-                "projectionTypeB": { ... }
-            },
-            "myOtherNamespace": {
-                "projectionTypeA": { ... }
-            }
-        }
-        */
+    const putProjectionState = (namespace, projectionName, state) => {
+        const ns = mockDB[namespace] = mockDB[namespace] || {};
+        const db = ns[projectionName] = ns[projectionName] || {};
+        db.state = typeof state === 'object' ? JSON.stringify(state) : state;
+        return Promise.resolve({});
     };
-    const appendEvent = (namespace, event) => {};
+
+    const getProjectionState = (namespace, projectionName) => {
+        const ns = mockDB[namespace] || {};
+        const { state = {} } = ns[projectionName] || {};
+        return Promise.resolve(typeof state === 'object'
+            ? state
+            : JSON.parse(state));
+    };
+
+    const getAll = () => {
+        return Promise.resolve(JSON.parse(JSON.stringify(mockDB)));
+    };
+    const appendEvent = (namespace, event) => {
+        const ns = mockDB[namespace] = mockDB[namespace] || {};
+        ns.events.push(event);
+        return Promise.resolve();
+    };
 
     return {
         projectionStates: {
